b03e37e45a363ef85e3eef8ae9ec81d017cbe023
THISISJUSTICE <yulop64@naver.com>
2023-08-31 10:54:15 +0900

    장해물의 기본 구현
    플레이어 및 장해물의 스텟 임시 추가

---
+++ Assets/Scripts/ObstacleBasic.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//장애물은 플레이어의 방어력이 더 높거나 높은 속도 혹은 데미지를 가지면 부술 수 있음
+public class ObstacleBasic : MonoBehaviour
+{
+    public Vector3 waitPos; //장애물이 대기 중일 때의 위치
+    public Vector3 appearPos; //장애물이 나타날 때의 위치
+    public float appearSpeed; //장애물이 나타나는 속도
+    public int appearFrame; //장애물이 나타나는 프레임
+    public int armor; //장애물의 방어력
+    public int magicRegistant; //장애물의 마법저항력
+
+    //정해진 범위에 플레이어가 들어오면 장해물 등장
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player") StartCoroutine(Appear());
+    }
+
+    //장애물 등장
+    IEnumerator Appear(){
+        for(int i=0; i<appearFrame; ++i){
+            transform.position += (appearPos - waitPos) / appearFrame;
+            yield return new WaitForSeconds(appearSpeed);
+        } 
+    }
+}
--- Assets/Scripts/PlayerBasic.cs
+++ Assets/Scripts/PlayerBasic.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class PlayerBasic : MonoBehaviour
 {
     #region Variable
-    public float speed; //플레이어 기본 속도
-    public float acceleration; //플레이어 돌진 속도
     public float lrPos; //플레이어의 좌우 칸마다의 이동 값
     public float lrMoveDelay; //플레이어의 좌우 이동 딜레이
     public int mapSpace = 5; // 맵의 좌우 칸 수(무조건 홀수)
 
+    public float speed; //플레이어 기본 속도
+    public float acceleration; //플레이어 돌진 속도
+    public int health; //플레이어 체력
+    public int armor; //플레이어 방어력
+    public int magicResitant; //플레이어 마법저항력
+    public int attackDamage; //플레이어 공격력
+    public int magicDamage; //플레이어 마법공격력
+    
+
     Rigidbody rigid;
     float[] lrSpace; //좌우 이동 위치
     int lrIndex; //현재 좌우 위치
-    bool lrMoving;
+    bool lrMoving; //현재 좌우 이동 중인지 확인
 
     #endregion
 
@@ -27,6 +34,7 @@ public class PlayerBasic : MonoBehaviour
         LRInit();
     }
 
+    //좌우 움직임 위치 설정
     void LRInit(){
         int temp = lrSpace.Length / 2; 
         float space = 0 - (lrPos * temp);
@@ -62,6 +70,7 @@ public class PlayerBasic : MonoBehaviour
         }
     }
 
+    //좌우 이동에 약간의 딜레이 설정
     IEnumerator LRMove(){
         gameObject.transform.position = new Vector3(lrSpace[lrIndex], gameObject.transform.position.y, gameObject.transform.position.z);
         lrMoving = true;
