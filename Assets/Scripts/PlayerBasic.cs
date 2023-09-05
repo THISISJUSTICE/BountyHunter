0ab11f09b66b09c677a42d582012a1fd6295fc6d
THISISJUSTICE <yulop64@naver.com>
2023-09-05 14:49:40 +0900

    장애물 감지 기능 추가
    좌우 이동 중 장애물이 있으면 이동 불가

--- Assets/Scripts/PlayerBasic.cs
+++ Assets/Scripts/PlayerBasic.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class PlayerBasic : MonoBehaviour
@@ -7,6 +8,7 @@ public class PlayerBasic : MonoBehaviour
     #region Variable
     public float lrPos; //플레이어의 좌우 칸마다의 이동 값
     public float lrMoveDelay; //플레이어의 좌우 이동 딜레이
+    public int lrMoveFrame; //플레이어의 좌우 이동 프레임
     public int mapSpace = 5; // 맵의 좌우 칸 수(무조건 홀수)
 
     [System.Serializable]
@@ -22,11 +24,9 @@ public class PlayerBasic : MonoBehaviour
     }
     public PlayerStatus playerStatus; //플레이어 스테이터스
     
-
     int curHealthPoint; //플레이어 현재 체력
     int curMagicPoint; //플레이어 현재 마나
     
-
     Rigidbody rigid;
     float[] lrSpace; //좌우 이동 위치
     int lrIndex; //현재 좌우 위치
@@ -34,12 +34,16 @@ public class PlayerBasic : MonoBehaviour
 
     #endregion
 
-    void Awake(){
+    private void Awake(){
+        PlayerBasicInit(); 
+    }
+
+    void PlayerBasicInit(){
         rigid = GetComponent<Rigidbody>();
         lrSpace = new float[mapSpace];
     }
 
-    void Start(){
+    private void Start(){
         LRInit();
     }
 
@@ -63,6 +67,7 @@ public class PlayerBasic : MonoBehaviour
     //↑: 가속, ↓: 멈춤, →: 오른쪽으로 한 칸 이동, ←: 왼쪽으로 한 칸 이동
     void Move(){
         float hor = Input.GetAxisRaw("Horizontal");
+        int lrTemp = lrIndex + (int)hor;
 
         //이동 및 가속
         if(!Input.GetKey(KeyCode.DownArrow)){
@@ -73,21 +78,38 @@ public class PlayerBasic : MonoBehaviour
         }
 
         //좌우 이동
-        if(hor != 0 && lrIndex + hor >= 0 && lrIndex + hor < lrSpace.Length && !lrMoving){
-            lrIndex += (int)hor;
-            StartCoroutine(LRMove());
+        if(hor != 0 && lrTemp >= 0 && lrTemp < lrSpace.Length && !lrMoving){
+            if(!ObstacleCheck(lrIndex, lrTemp)){
+                StartCoroutine(LRMove(lrIndex, lrTemp));
+                lrIndex = lrTemp;
+            }            
         }
     }
 
-    void ObstacleCheck(int start, int end){
-        int way = start > end ? 1 : -1;
+    //좌우 이동 중 이동하려는 방향에 레이를 쏴서 장애물이 있는지 확인
+    bool ObstacleCheck(int start, int end){
+        int way = start > end ? -1 : 1;
+        if(Physics.BoxCast(transform.position, transform.lossyScale * gameObject.GetComponent<CapsuleCollider>().radius * 0.9f, 
+        transform.right * way, out RaycastHit hit, transform.rotation, lrPos)){
+            Debug.Log(hit.collider.tag + "와 충돌");
+            if(hit.collider.tag == "Obstacle"){
+                Debug.Log("장애물 충돌");
+                return true;
+            }
+        }
+
+        return false;
     }
 
     //좌우 이동에 약간의 딜레이 설정
-    IEnumerator LRMove(){
+    IEnumerator LRMove(int start, int end){
+        float gap = (lrSpace[end] - lrSpace[start]) / lrMoveFrame; //프레임 당 이동 값
         gameObject.transform.position = new Vector3(lrSpace[lrIndex], gameObject.transform.position.y, gameObject.transform.position.z);
         lrMoving = true;
-        yield return new WaitForSeconds(lrMoveDelay);
+        for(int i=0; i<lrMoveFrame; ++i){
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x + gap, gameObject.transform.position.y, gameObject.transform.position.z);
+            yield return new WaitForSeconds(lrMoveDelay/lrMoveFrame);
+        }
         lrMoving = false;
     }
 }
