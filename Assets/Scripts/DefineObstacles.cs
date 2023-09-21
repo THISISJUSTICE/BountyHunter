417841461aa0595d6d8785a5171fe70830219d18
THISISJUSTICE <yulop64@naver.com>
2023-09-21 23:14:20 +0900

    장애물 데이터 양식 수정

--- Assets/Scripts/DefineObstacles.cs
+++ Assets/Scripts/DefineObstacles.cs
@@ -11,19 +11,18 @@ public class DefineObstacles : MonoBehaviour
 {
     [System.Serializable]
     public class Data{
-        public int kind; //장애물 종류
         public string prefabName; //원본 장애물의 프리팹 이름
-        public float[] waitPos; //대기 중일 때의 포지션 x,y,z
+        public int[] area; //차지하는 칸 수 가로, 세로
+        public float[] waitPos; //장애물이 대기 중일 때의 포지션 x,y,z
+        public float appearWaitTime; //장애물이 나타날 때 대기 시간
+        public float appearSpeed; //장애물이 나타나는 속도
+        public float appearheight; //장애물이 나타날 때의 높이
         public float[] objScale; //오브젝트 스케일 x, y, z
+        public float[] colPos; //충돌 콜라이더 포지션 x,y,z
         public float[] colScale; //충돌 콜라이더 스케일 x, y, z
         public int maxHealthPoint; //장애물의 최대 최력
         public int armor; //장애물의 방어력
         public int magicRegistant; //장애물의 마법 저항력
-
-        public Data(){
-            objScale = new float[3];
-            colScale = new float[3];
-        }
     }
     public Data[] rockObstacleData;
     string basicfileName = "ObstacleData"; //파일 명 앞에 장애물의 종류 붙이기
@@ -41,7 +40,8 @@ public class DefineObstacles : MonoBehaviour
         //클래스를 문자열로 된 Json 데이터로 변환
         string ToJsonData = "";
         for(int i=0; i<data.Length; ++i){
-            ToJsonData += JsonUtility.ToJson(data[i]) + "\n";
+            ToJsonData += JsonUtility.ToJson(data[i]);
+            if(i<data.Length-1) ToJsonData += "\n";
         }
         
         //Json 파일을 생성하고 저장(파일이 이미 있으면 덮어쓰기)
