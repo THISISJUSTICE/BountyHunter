d9567cb9ca587d986313494acca09c64521353d1
THISISJUSTICE <yulop64@naver.com>
2023-09-29 11:52:22 +0900

    총 발사 이펙트 기초 생성

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public int damage; //피해량
-    public float bulletSpeed; //총알 이동 속도
-    
+    public float speed; //총알 이동 속도
+    public float existTime; //총알이 존재하는 시간
+
+    private void OnEnable() {
+       
+    }
+
+    IEnumerator Disappear(){
+        yield return new WaitForSeconds(existTime);
+        gameObject.SetActive(false);
+    }
+
 }
